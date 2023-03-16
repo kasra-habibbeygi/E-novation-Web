7e7709b5893d08405929c9eb63cba617bb0d7540
kasra-habibbeygi <kasrahabibbeygi@gmail.com>
2023-03-16 14:14:12 +0330

    Create about us page

---
+++ src/assets/styles/about-us/index.jsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const MainField = styled.div({
+    borderRadius: '8px',
+    backgroundColor: 'white',
+    padding: '20px',
+    width: '100%',
+
+    h3: {
+        fontSize: '1.7rem'
+    },
+
+    p: {
+        marginTop: '20px',
+        fontSize: '1.1rem',
+        lineHeight: '30px'
+    }
+});
---
+++ src/pages/about-us.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
+// Component
+import LayoutProvider from '../components/layout/layout.provider';
+
+// Assets
+import { MainField } from '../assets/styles/about-us';
+
+const AboutUs = () => {
+    return (
+        <LayoutProvider>
+            <MainField>
+                <h3>About us</h3>
+                <p>
+                    Backed by the founders' extensive experience in Design and Manufacturing. We complement your needs with our extensive
+                    experience in Designing and Manufacturing Parts, Equipment, Tools and Machinery and will provide ongoing support during
+                    all stages of the Design, Development and Manufacturing process. At E-Novation Engineering, we specialize in providing
+                    advanced CNC machining services to a wide range of industries. You can rely on precision manufacturing delivered to the
+                    highest quality by highly experienced machinists (mostly qualified Engineers), using state-of-the-art machines, a
+                    controlled environment and stringent production standards. E-novation also offers the option of conducting engineering
+                    verification for defined applications of parts if customers so require. In everything E-Novation does, Safety comes
+                    first. There is nothing more important to us than sending our people home at the end of a working day safe and healthy.
+                    E-Novation has implemented the ISO 45001:2018 Occupational Health and Safety (OH&S) Management System to assist and
+                    guide our safety management practices. A combination of advanced and high quality CNC machine tools, top of the range
+                    CAD/CAM software, Integrated Quality management system and highly experienced staff are four pillars of our business and
+                    the reason we deliver the best results. Our Design and Manufacturing system is powered by Okuma CNC machines,
+                    SolidWorks, SolidCam, and ISO 9001 quality management system.
+                </p>
+            </MainField>
+        </LayoutProvider>
+    );
+};
+
+export default AboutUs;
